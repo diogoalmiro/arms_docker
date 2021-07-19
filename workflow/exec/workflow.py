df0e4eec5e08803f6a945f5608b13ef644c62802
João Vizoso <joao.vizoso@hotmail.com>
2021-07-19 16:59:11 +0100

    added --force

--- workflow/exec/workflow.py
+++ workflow/exec/workflow.py
@@ -13,15 +13,6 @@ import tesserocr
 import datetime
 import folderManager
 
-def get_batches():
-	batches = []
-	path = Path.cwd()/"tmp"
-	for batch in path.glob("*.JSON"):
-		batches.append(str(batch.stem))
-
-	return batches
-
-
 
 def to_do(file,lang,tmp,prep,comp,batch_name):
 	n = file['name']
@@ -77,44 +68,99 @@ def process(image,batch_name,lang,tmp,prep,comp):
 	print("Done")
 
 
+def exists(batch_name):
+	if((Path.cwd()/"tmp"/(str(batch_name+".JSON"))).exists()):
+		return True
+
+
+def forcing(path):
+	batch_name = str(path.parts[-1])
+	process = Path.cwd()/"tmp"/batch_name
+	if exists(batch_name):
+		for file in process.glob("*.JSON"):
+			Path(file).unlink()
+		docManager.delete_process(batch_name)
+
+
 @click.command()
 @click.argument('file')
 @click.option('--comp',is_flag=True,help="Disable line segmentation comparison", default=True,show_default=True)
 @click.option('--prep',is_flag=True,help="Additional prep-processing", default=False,show_default=True)
 @click.option('--lang',type=click.Choice(tesserocr.get_languages(str(Path.cwd().parents[0]/'tessdata'))[1], case_sensitive=False), multiple=True, show_default=True, default=('eng',),help="Available languages.")
-@click.option('--proc',type=click.Choice(get_batches(), case_sensitive=False), multiple=True,help="Unfinished processes.")
+@click.option('--force',is_flag=True,help="Start from the beggining", default=False,show_default=True)
 @click.option('--tmp', is_flag=True ,help="Keep tmp files. WARNING: It requires more free disk space", default=False, show_default=True)
 @click.option('--folder', is_flag=True ,help="Workflow is performed in all images from this folder", default=False, show_default=True)
 
 
 
-def main(file,lang,tmp,folder,prep,comp,proc):
+def main(file,lang,tmp,folder,prep,comp,force):
 	files = []
-	
-	if (proc):
-		batch_name = proc[0]
-		files = docManager.get_files(batch_name)
-
-	else:
-		batch_name = "batch_" + str(datetime.datetime.now().timestamp())
-		folderManager.create_TMP_B(batch_name)
-		folderManager.create_RESULT_B(batch_name)
-		docManager.write_process(batch_name)
-
 	if (folder):
+		
 		path = Path(file)
-		for image in path.glob("*.tif"):
-			if str(image) not in files:
-				image.rename(image.with_suffix(".tiff"))
+		batch_name = str(path.parts[-1])
+		
+		if (force):
+			forcing(path)
+			folderManager.create_TMP_B(batch_name)
+			folderManager.create_RESULT_B(batch_name)
+			docManager.write_process(batch_name)
+			
+			for image in path.glob("*.tif"):
+					image.rename(image.with_suffix(".tiff"))
 
-		for image in path.glob("*.tiff"):
-			if str(image) not in files:
-				process(image,batch_name,lang,tmp,prep,comp)
+			for image in path.glob("*.tiff"):
+					process(image,batch_name,lang,tmp,prep,comp)
+		
+		else: 
+			if exists(batch_name):
+				
+				print("{} already exists and the process will continue".format(batch_name))
+				print("To start again, use --force ")
+				files = docManager.get_files(batch_name)
+				
+				for image in path.glob("*.tif"):
+					if str(image) not in files:
+						image.rename(image.with_suffix(".tiff"))
+
+				for image in path.glob("*.tiff"):
+					if str(image) not in files:
+						process(image,batch_name,lang,tmp,prep,comp)
+			else: 
+				
+				folderManager.create_TMP_B(batch_name)
+				folderManager.create_RESULT_B(batch_name)
+				docManager.write_process(batch_name)
+				
+				for image in path.glob("*.tif"):
+						image.rename(image.with_suffix(".tiff"))
+
+				for image in path.glob("*.tiff"):
+						process(image,batch_name,lang,tmp,prep,comp)
 
 	else:
-		if image not in files:
-			process(image,batch_name,lang,tmp,prep,comp)
+		path = Path(file)
+		batch_name = str(path.parts[-1])
 
+		if (force):
+			forcing(path)
+			folderManager.create_TMP_B(batch_name)
+			folderManager.create_RESULT_B(batch_name)
+			docManager.write_process(batch_name)
+			process(path,batch_name,lang,tmp,prep,comp)
+
+		else: 
+			if exists(batch_name):
+				print("{} already exists and the process will continue".format(batch_name))
+				print("To start again, use --force ")
+				files = docManager.get_files(batch_name)
+				if str(file not in files):
+					process(file,batch_name,lang,tmp,prep,comp)
+			else: 
+				folderManager.create_TMP_B(batch_name)
+				folderManager.create_RESULT_B(batch_name)
+				docManager.write_process(batch_name)
+				process(path,batch_name,lang,tmp,prep,comp)
 
 	docManager.delete_process(batch_name)
 
